f113e7d8e4bd73b0eddc9391ca4669cc99831469
bluolightning <124224350+bluolightning@users.noreply.github.com>
2025-06-19 18:05:08 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+javascript: (function () {
+    'use strict';
+    function e(t, o, i = 5e3) {
+        let l = 0;
+        const a = 100,
+            n = setInterval(function () {
+                const s = document.querySelector(t);
+                s ? (clearInterval(n), o(s)) : (l += a) >= i && (clearInterval(n), o(null));
+            }, a);
+    }
+    console.log('Lichess Export: Starting...');
+    const t = document.querySelector(
+        '.icon-font-chess.share.live-game-buttons-button, [data-cy="share-button"], .share-button-component'
+    );
+    if (!t)
+        return alert(
+            'Lichess Export: Could not find the Share button. Try running this on the game over screen.'
+        );
+    t.click(),
+        e('#tab-pgn', function (t) {
+            if (!t) return alert('Lichess Export: Could not find the PGN tab in the Share menu.');
+            t.click(),
+                e('.share-menu-content textarea, [data-cy="share-pgn-textarea"]', function (t) {
+                    let o = t ? t.value : null;
+                    if (!o) {
+                        const t = document.querySelector('.share-menu-content [pgn]');
+                        t && (o = t.getAttribute('pgn'));
+                    }
+                    if (!o)
+                        return alert(
+                            'Lichess Export: Could not extract PGN data. Chess.com may have updated their site.'
+                        );
+                    const i = 'https://lichess.org/paste?pgn=' + encodeURIComponent(o);
+                    window.open(i, '_blank');
+                    const l = document.querySelector('#share-modal .cc-close-button-component');
+                    l && l.click();
+                });
+        });
+})();
